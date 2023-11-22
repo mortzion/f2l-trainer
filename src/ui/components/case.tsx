1afd588ddc046c6e74ea8c796abda6fb051ca3c0
Matheus Prachedes Batista <matheusprachedesb@gmail.com>
2023-11-21 22:25:56 -0300

    fixed f2l case image

--- src/ui/components/case.tsx
+++ src/ui/components/case.tsx
@@ -10,7 +10,7 @@ export function Case(props: CaseProps) {
   return (
     <div className={classNames("flex border-black border flex-grow")} style={{ marginLeft: -1, marginTop: -1 }}>
       <div className="border-r border-black">
-        <img src={`/cases/${props.case.picture}`} width={64} height={64} />
+        <img src={`./cases/${props.case.picture}`} width={64} height={64} />
       </div>
       <div className="px-1">
         {props.case.algs.map((alg, index) => (
